1c97cdb963cca79e1b525780be7c84ab32328bf2
TCAA-Web <kfry@techcollege.com>
2025-08-08 09:01:41 +0200

    removed old cat - added new cat

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,1 @@
 "use strict";
-let banner = document.getElementById('banner');
-let timeout = setTimeout(() => {
-    banner === null || banner === void 0 ? void 0 : banner.classList.add('hidden');
-}, 6000);
--- scripts/main.ts
+++
@@ -1,5 +0,0 @@
-let banner = document.getElementById('banner')
-
-let timeout = setTimeout(() => {
-  banner?.classList.add('hidden')
-}, 6000)
